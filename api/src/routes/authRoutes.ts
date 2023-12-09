c3f1b7b2f0131d94e71c1637ff2c8113d85d36c2
thownef <vantho3042000@gmail.com>
2023-12-09 15:30:25 +0700

    change source client to branch client

--- api/src/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express, { Router } from "express";
-import { login, register } from "../controllers/authControllers";
-
-const router: Router = express.Router();
-
-router.post("/register", register);
-router.post("/login", login);
-
-export default router;
