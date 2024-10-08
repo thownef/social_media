7234daa02a952cb42135a0eea1b7fb81909e9b77
VJD - Tho Nguyen <tho.nguyen@vj-partner.com>
2024-10-08 16:56:14 +0700

    update route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,11 @@
-import { isEmpty } from "lodash";
-import { createBrowserRouter, RouteObject, RouterProvider, useRoutes } from "react-router-dom";
-import { lazy, Suspense, useEffect } from "react";
+import { createBrowserRouter, RouteObject } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
 import { ModuleName, PageName, PagePath } from "@/shared/core/enum/page.enum";
 import { lazyLoadModuleRoute, lazyLoadRoute } from "@/routes/LazyLoadRoutes";
 import ValidateLoginRoute from "@/routes/ValidateLoginRoute";
 import PrivateRoute from "@/routes/PrivateRoute";
-import { RootState } from "@/shared/store";
 
-import { useAppDispatch, useAppSelector } from "@/shared/hooks/useAppHooks";
-import { setUser } from "@/shared/store/authSlice";
 import Layout from "@/shared/layouts";
 
 const NavigateComponent = lazy(() => import("@/shared/components/Navigate/Navigate"));
@@ -43,33 +39,6 @@ const configRoutes: RouteObject[] = [
   },
 ];
 
-const RoutesApp = () => {
-  const dispatch = useAppDispatch();
-  const profile = useAppSelector((state: RootState) => state.user);
-  const isAuthenticated = localStorage.getItem("bearer_token");
-  const router = createBrowserRouter(configRoutes);
+const router = createBrowserRouter(configRoutes);
 
-  useEffect(() => {
-    (async () => {
-      if (isAuthenticated && !profile) {
-        try {
-          // const res = await getProfile();
-          // const profile = res?.data?.data || {};
-          const profile = null;
-
-          if (isEmpty(profile)) {
-            localStorage.removeItem("bearer_token");
-          } else {
-            dispatch(setUser(profile));
-          }
-        } catch (error) {}
-      }
-    })();
-  }, []);
-
-  return <>
-    <RouterProvider router={router} />;
-  </>
-};
-
-export default RoutesApp;
+export default router;
