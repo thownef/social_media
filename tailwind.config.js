c41e6cc32bf5b52924fe98bf2231308f56c0a2c7
thownef <vantho3042000@gmail.com>
2024-12-25 23:03:12 +0700

    back

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,7 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {
-            backgroundImage: {
-                'upper-right': "url('/assets/img/buttom-left.webp')",
-                'bottom-left': "url('/assets/img/buttom-left.webp')",
-            }
-        }
+        extend: {}
     },
     plugins: [],
 };
