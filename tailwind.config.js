6e78a3d7d0479cd972007b869525c7a08e288db8
thownef <vantho3042000@gmail.com>
2024-12-25 23:01:51 +0700

    update UI form login

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: {
+                'upper-right': "url('/assets/img/buttom-left.webp')",
+                'bottom-left': "url('/assets/img/buttom-left.webp')",
+            }
+        }
     },
     plugins: [],
 };
