f740b1eea4e31976ff9a58125566d2711b7f4909
thownef <vantho3042000@gmail.com>
2025-01-15 15:01:48 +0700

    update validate post

--- src/modules/home/core/config/form/init-form-post.ts
+++ src/modules/home/core/config/form/init-form-post.ts
@@ -6,16 +6,29 @@ export const initFormPost: PostForm = {
   files: [],
 };
 
-export const PostFormSchema = Yup.object().shape({
-  content: Yup.string().optional(),
-  files: Yup.array().transform((value) => {
-    if (value && !Array.isArray(value)) {
-      return Object.values(value);
-    }
-    return value;
-  }).of(
-    Yup.mixed().nullable()
-  ).optional(),
+export const PostFormSchema = Yup.lazy((obj) => {
+  return Yup.object({
+    content: Yup.string()
+      .trim()
+      .when([], {
+        is: () => obj.files || obj.files.length === 1,
+        then: (schema) => schema.optional(),
+        otherwise: (schema) => schema.required(),
+      }),
+    files: Yup.array()
+      .transform((value) => {
+        if (value && !Array.isArray(value)) {
+          return Object.values(value);
+        }
+        return value;
+      })
+      .of(Yup.mixed().nullable())
+      .when([], {
+        is: () => !obj.content || obj.content.trim() === "",
+        then: (schema) => schema.min(1, "Bạn phải nhập nội dung hoặc đính kèm tệp tin"),
+        otherwise: (schema) => schema.optional(),
+      }),
+  });
 });
 
 export type FormPost = Yup.InferType<typeof PostFormSchema>;
