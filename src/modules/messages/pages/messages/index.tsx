0c7211bdf2fa1988ae81427b3256e9264c11b39b
VJD - Tho Nguyen <tho.nguyen@vj-partner.com>
2025-02-14 16:06:53 +0700

    update config laravel-echo

--- src/modules/messages/pages/messages/index.tsx
+++ src/modules/messages/pages/messages/index.tsx
@@ -1,7 +1,24 @@
-const MessagesPage = () => {
-  return (
-    <div>MessagePage</div>
-  )
-}
+import { echo } from "@/shared/utils/echo";
+import { useEffect } from "react";
 
-export default MessagesPage
+const MessagesPage = () => {
+  useEffect(() => {
+    echo.join('online')
+      .here((users: any) => {
+        console.log("here", users);
+      })
+      .joining((user: any) => {
+        console.log("joining", user);
+      })
+      .leaving((user: any) => {
+        console.log("leaving", user);
+      })
+      .error((error: any) => {
+        console.log("error", error);
+      });
+  }, []);
+
+  return <div>MessagePage</div>;
+};
+
+export default MessagesPage;
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-    "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
-},
-})
+  server: {
+    proxy: {
+      '/broadcasting': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+    }
+  }
+});
