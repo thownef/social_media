dbe4b871ff8d87b6ab43170ddf1b204128d2d3c2
thownef <vantho3042000@gmail.com>
2025-02-21 15:14:15 +0700

    get token from cookie

--- src/shared/store/authSlice.ts
+++ src/shared/store/authSlice.ts
@@ -16,10 +16,12 @@ export const userSlice = createSlice({
     login: (state, action) => {
       const { user, accessToken } = action.payload;
       localStorage.setItem("accessToken", accessToken);
+      document.cookie = `accessToken=${accessToken}; max-age=${7 * 24 * 60 * 60}; path=/`;
       state.user = user;
     },
     logout: (state) => {
       localStorage.removeItem("accessToken");
+      document.cookie = "accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
       state.user = null;
     },
     setUser: (state, action) => {
