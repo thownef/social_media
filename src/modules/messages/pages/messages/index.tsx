23d4d4b4b9e868e16ba54f98feefa3445c88e009
VJD - Tho Nguyen <tho.nguyen@vj-partner.com>
2025-02-21 16:40:35 +0700

    event online

--- src/modules/messages/pages/messages/index.tsx
+++ src/modules/messages/pages/messages/index.tsx
@@ -1,17 +1,24 @@
+import { useEffect, useState } from "react";
 import { echo } from "@/shared/utils/echo";
-import { useEffect } from "react";
+import { User } from "@/shared/core/types";
 
 const MessagesPage = () => {
+  const [onlineUsers, setOnlineUsers] = useState<Record<string, User>>({});
+  console.log(onlineUsers);
   useEffect(() => {
     echo.join('online')
       .here((users: any) => {
-        console.log("here", users);
+        console.log(users)
       })
       .joining((user: any) => {
-        console.log("joining", user);
+        setOnlineUsers((prev) => ({ ...prev, [user.id]: user }));
       })
       .leaving((user: any) => {
-        console.log("leaving", user);
+        setOnlineUsers((prev) => {
+          const newUsers = { ...prev };
+          delete newUsers[user.id];
+          return newUsers;
+        });
       })
       .error((error: any) => {
         console.log("error", error);
