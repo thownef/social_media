513334673885fad41261e77d4a3c2ac4b7883e9f
thownef <vantho3042000@gmail.com>
2025-05-18 20:27:14 +0700

    update config echo

--- src/shared/utils/echo.ts
+++ src/shared/utils/echo.ts
@@ -4,17 +4,18 @@ import Pusher from "pusher-js";
 window.Pusher = Pusher;
 
 export const echo = new Echo({
-  broadcaster: 'reverb',
+  broadcaster: "reverb",
   key: import.meta.env.VITE_APP_REVERB_APP_KEY,
   wsHost: import.meta.env.VITE_APP_REVERB_HOST,
   wsPort: import.meta.env.VITE_APP_REVERB_PORT ?? 80,
   wssPort: import.meta.env.VITE_APP_REVERB_PORT ?? 443,
-  forceTLS: (import.meta.env.VITE_APP_REVERB_SCHEME ?? 'https') === 'https',
-  enabledTransports: ['ws', 'wss'],
-  authEndpoint: "/broadcasting/auth",
+  forceTLS: (import.meta.env.VITE_APP_REVERB_SCHEME ?? "https") === "https",
+  enabledTransports: ["ws", "wss"],
+  authEndpoint: "http://localhost:8000/broadcasting/auth",
   auth: {
     headers: {
-      Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+      Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      Accept: "application/json",
     },
   },
 });
