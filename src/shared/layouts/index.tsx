a67579075b11f33761938b44c008972a3158b03c
thownef <vantho3042000@gmail.com>
2025-09-02 13:37:37 +0700

    change layout

--- src/shared/layouts/index.tsx
+++ src/shared/layouts/index.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div className="bg-[var(--background-color)]">
+    <div className="bg-[var(--background)]">
       <Navbar />
       <div>
         <Outlet />
